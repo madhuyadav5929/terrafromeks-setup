872dd7da1a5d004629415e7c6dfa6721414446c3
madhuyadv5929 <udarimadhu0@gmail.com>
2024-05-14 17:26:05 +0530

    tasks

--- 02-task4/main.tf
+++ 02-task4/main.tf
@@ -68,7 +68,7 @@ module "ec2_instance" {
   name = "Jenkins-Server"
 
   instance_type               = var.instance_type
-  key_name                    = "siva"
+  key_name                    = "aws-practice"
   monitoring                  = true
   vpc_security_group_ids      = [module.sg.security_group_id]
   subnet_id                   = module.vpc.public_subnets[0]
